3dc9c7d885f301c56f6dc5f8f12b752b3d34ad0c
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-06-01 05:02:15 +0300

    #2

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,19 @@
+'about': {},
+
+'comein': {},
+
+'head': {
+    defaults: {
+        title: 'Dima Kovpak',
+        useSocialMetaTags: false
+    }
+},
+
+'master': {},
+
+'who': {},
+
+__pages: [{
+                name: 'index',
+                href: 'index.html'
+             }]
